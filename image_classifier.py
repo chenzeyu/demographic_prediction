d54f8a13cc0764cca21bc1fc94f39968692d8076
syiham <indrakesuma.hisyam@gmail.com>
2015-03-22 15:59:47 +0800

    add image_classifier

---
+++ image_classifier.py
@@ -0,0 +1,168 @@
+import base64
+import csv
+import gc
+import time
+try:
+   import cPickle as pickle
+except:
+   import pickle
+
+import numpy as np
+
+from sklearn.decomposition import RandomizedPCA
+from sklearn.decomposition import SparseCoder
+from sklearn.decomposition import DictionaryLearning
+
+from sklearn.linear_model import LogisticRegression
+from sklearn import cross_validation
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import BernoulliRBM
+
+import skimage.io as io
+import skimage.transform as trans
+
+
+# Initialize
+#########################################
+START = time.time()
+#########################################
+
+
+# Get user info
+#########################################
+userMap = {}
+
+f = open("train.csv")
+reader = csv.reader(f, 'excel')
+
+reader.next()
+
+for row in reader:
+	userMap[row[0]] = (row[1], row[2])
+
+f.close()
+#########################################
+
+
+# Get image data
+#########################################
+start = time.time()
+print "Start getting image data", time.clock()
+
+f = open("image_features.bin", "rb")
+oriData = pickle.load(f)
+f.close()
+
+f = open("image_fileMap.bin", "rb")
+fileMap = pickle.load(f)
+f.close()
+
+genders = []
+ages = []
+both = []
+
+train = []
+test = []
+
+for i in range (0, 4113):
+	userId = fileMap[i]
+	try:
+		gender, age = userMap[userId]
+		genders.append(gender)
+		ages.append(age)
+		both.append(gender + " # " + age)
+		train.append(oriData[i])
+	except:
+		test.append(oriData[i])
+	
+train = np.array(train)
+test = np.array(test)
+
+
+size = len(train)
+end = time.time()
+
+print "End getting image data", time.clock()
+
+print "Elapsed Time:", end-start
+#########################################
+
+
+# Do feature selection/extraction(PCA)
+#########################################
+decomp = RandomizedPCA(n_components = 200)
+train  = decomp.fit_transform(train)
+#########################################
+
+
+# Initialize Classifier(SVC)
+#########################################
+genderClassifier = GradientBoostingClassifier()
+ageClassifier = GradientBoostingClassifier()
+bothClassifier = GradientBoostingClassifier()
+
+end = time.time()
+#########################################
+
+# genderClassifier.fit(train, genders)
+# ageClassifier.fit(train, ages)
+
+# with open('image_classifiers.bin', 'wb') as fp:
+# 	pickle.dump(genderClassifier, fp)
+# 	pickle.dump(ageClassifier, fp)
+# fp.close()
+
+# Run 10-Fold Validation
+#########################################
+cv = cross_validation.KFold(size, n_folds=10)
+resultsGender = []
+resultsAge = []
+resultsBoth = []
+i = 1
+for traincv, testcv in cv:
+	print "Starting iteration ", i
+	start = time.time()
+
+	s = time.time()
+	genderClassifier.fit(train[traincv[0]:traincv[-1]], genders[traincv[0]:traincv[-1]])
+	ageClassifier.fit(train[traincv[0]:traincv[-1]], ages[traincv[0]:traincv[-1]])
+	bothClassifier.fit(train[traincv[0]:traincv[-1]], both[traincv[0]:traincv[-1]])
+	e = time.time()
+
+	print "Classifier Building Time: ", e-s
+
+	gP = genderClassifier.predict(train[testcv[0]:testcv[-1]])
+	aP = ageClassifier.predict(train[testcv[0]:testcv[-1]])
+	bP = bothClassifier.predict(train[testcv[0]:testcv[-1]])
+	resultsGender.append( accuracy_score(genders[testcv[0]:testcv[-1]], gP,normalize=True ))
+	resultsAge.append( accuracy_score(ages[testcv[0]:testcv[-1]], aP,normalize=True) )
+	resultsBoth.append( accuracy_score(both[testcv[0]:testcv[-1]], bP,normalize=True) )
+
+	print "Iteration ", i , "results:"
+	print "Gender Accuracy: ", resultsGender[i-1]
+	print classification_report(genders[testcv[0]:testcv[-1]], gP), "\n"
+	print "Age Accuracy: ", resultsAge[i-1]
+	print classification_report(ages[testcv[0]:testcv[-1]], aP), "\n"
+	print "Combined Accuracy: ", resultsBoth[i-1]
+	print classification_report(both[testcv[0]:testcv[-1]], bP), "\n"
+
+	end = time.time()
+	print "Elapsed Time for iteration ", i, " : ", end-start
+	print "\n"
+	i+=1
+	gc.collect()
+
+print "Gender Accuracy Results: " + str( np.array(resultsGender).mean() )
+print "Age Accuracy Results: " + str( np.array(resultsAge).mean() )
+print "Combined Accuracy Results: " + str( np.array(resultsBoth).mean() )
+
+END = time.time()
+print "Finished, Total Elapsed Time: ", END-START
+#########################################
+
+
---
+++ image_classifier_lib.py
@@ -0,0 +1,174 @@
+import base64
+import csv
+import gc
+import time
+try:
+   import cPickle as pickle
+except:
+   import pickle
+
+import numpy as np
+
+from sklearn.decomposition import RandomizedPCA
+
+from sklearn.linear_model import LogisticRegression
+from sklearn import cross_validation
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import BernoulliRBM
+
+import skimage.io as io
+import skimage.transform as trans
+
+def predict(userIds):
+	f = open("image_features.bin", "rb")
+	oriData = pickle.load(f)
+	f.close()
+
+	f = open("image_fileMap.bin", "rb")
+	fileMap = pickle.load(f)
+	f.close()
+
+	f - open("image_classifiers.bin", "rb")
+	genderClassifier = pickle.load(f)
+	ageClassifier = pickle.load(f)
+	f.close()
+
+	test = []
+	user = []
+
+	for i in range (0, 4113):
+		userId = fileMap[i]
+		if userId in userIds:
+			user.append(userId)
+			test.append(oriData[i])
+
+	gP = genderClassifier.predict(test)
+	aP = ageClassifier.predict(test)
+
+	result = []
+
+	for u in userIds:
+		male = 0
+		fem = 0
+		a = 0
+		b = 0
+		c = 0
+		d = 0
+		e = 0
+		for i in range(0, len(user)):
+			if u == user[i]:
+				if gP[i] == "MALE":
+					male+=1
+				else:
+					fem+=1
+
+				age = aP[i]
+				if age == "18-24":
+					a+=1
+				elif age == "25-34":
+					b+=1
+				elif age == "35-49":
+					c+=1
+				elif age == "50-64":
+					d+=1
+				else:
+					e+=1
+
+		if fem > male:
+			gender = "FEMALE"
+		else:
+			gender = "MALE"
+
+		aList = [(a,"18-24"),(b, "25-34"),(c, "35-49"),(d, "50-64"),(e, "65-xx")]
+		aList.sort()
+		age = aList[4][1]
+		result.append((gender, age))
+
+	return result
+
+
+def train_and_predict(userIds, genders, ages, targets):
+	f = open("image_features.bin", "rb")
+	oriData = pickle.load(f)
+	f.close()
+
+	f = open("image_fileMap.bin", "rb")
+	fileMap = pickle.load(f)
+	f.close()
+
+	userMap = {}
+	train = []
+	test = []
+	user = []
+
+	decomp = RandomizedPCA(n_components = 200)
+	oriData  = decomp.fit_transform(oriData)
+
+	for i in range(0, len(userIds)):
+		userMap[userIds[i]] = (genders[i], ages[i])
+
+	for i in range (0, 4113):
+		userId = fileMap[i]
+		if userId in userIds:
+			train.append(oriData[i])
+		elif userId in targets:
+			user.append(userId)
+			test.append(oriData[i])
+
+	train = np.array(train)
+	test = np.array(test)
+
+	genderClassifier = GradientBoostingClassifier()
+	ageClassifier = GradientBoostingClassifier()
+
+	genderClassifier.fit(train, genders)
+	ageClassifier.fit(train, ages)
+
+	gP = genderClassifier.predict(test)
+	aP = ageClassifier.predict(test)
+
+	result = []
+
+	for u in targets:
+		male = 0
+		fem = 0
+		a = 0
+		b = 0
+		c = 0
+		d = 0
+		e = 0
+		for i in range(0, len(user)):
+			if u == user[i]:
+				if gP[i] == "MALE":
+					male+=1
+				else:
+					fem+=1
+
+				age = aP[i]
+				if age == "18-24":
+					a+=1
+				elif age == "25-34":
+					b+=1
+				elif age == "35-49":
+					c+=1
+				elif age == "50-64":
+					d+=1
+				else:
+					e+=1
+
+		if fem > male:
+			gender = "FEMALE"
+		else:
+			gender = "MALE"
+
+		aList = [(a,"18-24"),(b, "25-34"),(c, "35-49"),(d, "50-64"),(e, "65-xx")]
+		aList.sort()
+		age = aList[4][1]
+		result.append((gender, age))
+
+	return result
